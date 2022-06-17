a1e42aa660434b60af13bc46379c5f987b70b4a7
halilesmer <halil@esmer.de>
2022-06-17 05:43:52 +0200

    slides.length

--- src/components/Slides.js
+++ src/components/Slides.js
@@ -2,39 +2,38 @@ import React, { useState } from 'react';
 
 function Slides({ slides }) {
     const [count, setCount] = useState(0)
-    console.log('count', count)
+    console.log('count', slides.length)
     const HandleCountNext = () => {
-        if (count === 4) {
-            setCount(4)
+        if (count > slides.length) {
+            setCount(slides.length)
         } else {
             setCount(count + 1)
         }
     }
-    const HandleCountPrev =()=>{
-        if (count===0) {
+    const HandleCountPrev = () => {
+        if (count === 0) {
             setCount(0)
-        } else{
-            setCount( count -1)
+        } else {
+            setCount(count - 1)
         }
-        
+
     }
     return (
         <div>
             <div id="navigation" className="text-center">
-                <button disabled={count===0} data-testid="button-restart" className="small outlined"
+                <button disabled={count === 0} data-testid="button-restart" className="small outlined"
                     onClick={() => setCount(0)}
                 >Restart</button>
                 <button
-                disabled={count===0}
+                    disabled={count === 0}
                     data-testid="button-prev" className="small"
-                    onClick={HandleCountPrev}
-
-                >Prev
+                    onClick={HandleCountPrev}>Prev
                 </button>
                 <button
-                 disabled={count===4}
-                  data-testid="button-next" className="small"
-                    onClick={HandleCountNext}>Next</button>
+                    disabled={count === 4}
+                    data-testid="button-next" className="small"
+                    onClick={HandleCountNext}>Next
+                </button>
             </div>
 
             <div id="slide" className="card text-center">
